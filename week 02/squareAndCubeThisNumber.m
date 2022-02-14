395b78a53bd8316828f1b56635d6ce0376582b0f
Lucky <deepankarsharma2003@gmail.com>
2022-02-14 11:16:36 +0530

    Finished week 01

---
+++ week 02/squareAndCubeThisNumber.m
@@ -0,0 +1,4 @@
+function [y1, y2]= squareAndCubeThisNumber(x)
+% returns elementwise square and cube of X
+y1= x.^2;
+y2= x.^3;
---
+++ week 02/squareThisNumber.m
@@ -0,0 +1,4 @@
+function [y1]= squareThisNumber(x)
+% returns elementwise square and cube of X
+y1= x.^2;
+%y2= x.^3;
